a4df17448bd5ca925239c8faa38bd4ca179d54f7
Webhero9297 <webhero9297@mail.com>
2017-08-22 10:20:31 -0500

    webhero9297

--- dev_dir/truffle_proj001/Ethereum/contracts/HelloWorld.sol
+++ dev_dir/truffle_proj001/Ethereum/contracts/HelloWorld.sol
@@ -1,18 +1,33 @@
 pragma solidity ^0.4.0;
 contract HelloWorld {
     
-    uint public balance;
+    address public owner;
+    //uint public balance;
     mapping (address => uint) balances;
     
     // constructor
     // runs once upon contract creation
     
     function HelloWorld() {
-        balance = 1000;
+        owner = msg.sender;
+        balances[owner] = 1000;
     }
 
+    function transfer( address _to, uint _value) returns (bool success) {
+        if ( balances[msg.sender] < _value ) {
+            return false;
+        }
+        balances[msg.sender] -= _value;
+        balances[_to] += _value;
+        return true;
+    }
+    function getBalance(address _user) constant returns (uint _balance) {
+        return balances[_user];
+    }
+/*
     function deposit(uint _value) returns(uint _newValue) {
         balance += _value;
         return balance;
     }
+*/
 }
